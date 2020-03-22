697808cb36aff1b4dddfb68e737899700e89de0c
Andre Skeene <halodre24@gmail.com>
2020-03-22 17:03:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
       student = Student.new
       student.name = doc.css(".card-text-container").css("h4").text
       student.location = doc.css(".card-text-container").css("p").text
-      student.profile_url = doc.css(".student-card").css("a").first.["href"]
+      student.profile_url = doc.css(".student-card").css("a").first["href"]
     binding.pry
   end
 
