9f00ed34076074a544a3981a14f5263a9d47e76f
Andre Skeene <halodre24@gmail.com>
2020-03-22 17:13:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,11 +11,11 @@ class Scraper
     students = []
 
     doc.css(".card-text-container").each do |card|
-      students << {
+      students << 
       :name => card.css(".card-text-container").css("h4").text
       :location => card.css(".card-text-container").css("p").text
       :profile_url => card.css(".student-card").css("a").first["href"]
-    }
+
     # binding.pry
     end
     students
@@ -24,5 +24,6 @@ class Scraper
   def self.scrape_profile_page(profile_url)
 
   end
+
 
 end
