239bb5dd682ff5881ac3552eca5e0b7f56840d1e
Andre Skeene <halodre24@gmail.com>
2020-03-22 17:17:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,27 +6,28 @@ class Scraper
   @@all = []
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
+    html = open(index_url)
 
-    students = []
+        page = Nokogiri::HTML(html)
 
-    doc.css(".card-text-container").each do |card|
-      html = open(index_url)
+        students = []
 
-          page = Nokogiri::HTML(html)
+        page.css("div.student-card").each do |student|
 
-          students = []
+          students << {
+             :name => student.css("h4.student-name").text,
+             :location => student.css("p.student-location").text,
+             :profile_url => student.children[1].attributes["href"].value
+          }
+        end
+        students
 
-          page.css("div.student-card").each do |student|
-
-            students << {
-               :name => student.css("h4.student-name").text,
-               :location => student.css("p.student-location").text,
-               :profile_url => student.children[1].attributes["href"].value
-            }
-          end
-          students
-
+    # doc = Nokogiri::HTML(open(index_url))
+    #
+    # students = []
+    #
+    # doc.css(".card-text-container").each do |card|
+    #
     #   :name => card.css(".card-text-container").css("h4").text
     #   :location => card.css(".card-text-container").css("p").text
     #   # :profile_url => card.css(".student-card").css("a").first["href"]
