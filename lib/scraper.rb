89b8d29618c62dae1552067c14abf03c676e2fee
Andre Skeene <halodre24@gmail.com>
2020-03-22 17:09:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,9 +11,9 @@ class Scraper
     students = []
 
     doc.css(".card-text-container").each do |card|
-      :name = doc.css(".card-text-container").css("h4").text
-      :location = doc.css(".card-text-container").css("p").text
-      :profile_url = doc.css(".student-card").css("a").first["href"]
+      :name => doc.css(".card-text-container").css("h4").text
+      :location => doc.css(".card-text-container").css("p").text
+      :profile_url => doc.css(".student-card").css("a").first["href"]
     # binding.pry
     end
   end
